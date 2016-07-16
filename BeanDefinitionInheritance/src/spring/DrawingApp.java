672877dc80e4044d6e9c990e31fee3f0ef2f5814
mandeep147 <mndpkaur14@gmail.com>
2016-07-16 15:20:49 +0530

    bean inheritance

---
+++ BeanDefinitionInheritance/src/spring/DrawingApp.java
@@ -0,0 +1,16 @@
+package spring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+@SuppressWarnings("deprecation")
+public class DrawingApp {
+	public static void main(String []args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle  = (Triangle) context.getBean("triangle2"); 	
+		triangle.draw();		
+	}
+}
